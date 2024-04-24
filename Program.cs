ed12a0859cdb799a504a0779643f8f27954be2ff
unknown <guilhermedealmeidasilva17@gmail.com>
2024-04-24 12:56:43 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿const string SenhaCorreta = "Gui@1708";
+string senha, permissaoAcesso;
+
+Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+senha = Console.ReadLine()!;
+
+permissaoAcesso = senha == SenhaCorreta ? "permitido" : "negado";
+
+Console.WriteLine($"Acesso {permissaoAcesso}");
